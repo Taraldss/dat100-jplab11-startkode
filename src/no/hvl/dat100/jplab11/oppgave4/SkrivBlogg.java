5e8ee277f2ec48290cb9edb989dc792a2f7a69ca
Tarald Skaar <tarald.skaar@gmail.com>
2020-11-01 12:40:05 +0100

    fiksa feil, oppgave ferdig.

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -1,8 +1,7 @@
 package no.hvl.dat100.jplab11.oppgave4;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 import no.hvl.dat100.jplab11.common.TODO;
 import no.hvl.dat100.jplab11.oppgave3.*;
@@ -10,34 +9,23 @@ import no.hvl.dat100.jplab11.oppgave3.*;
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
-		
-		boolean erGyldig = false;
-
+		Boolean written = false;
 		try {
-
-		File file = new File(mappe + filnavn);
-		BufferedReader read = new BufferedReader(new FileReader(file));
-		
-		StringBuilder content = new StringBuilder();
-		String line = samling.toString();
-
-		while ((line = read.readLine()) != null) {
-	        content.append(line);
-	        content.append(System.lineSeparator());
-	    }
-		System.out.println(content);
-		
-		erGyldig = true;
-		
-		read.close();
-		return erGyldig;
-		}catch(Exception FileNotFoundException){
-			System.out.println("This file does not exist");
-			erGyldig = false;
+			PrintWriter write = new PrintWriter(mappe+filnavn);
 			
-			return erGyldig;
+			String data = samling.toString();
 			
+			write.write(data);
+			written = true;
+			write.close();
+
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
+		return written;
 	}
 }
